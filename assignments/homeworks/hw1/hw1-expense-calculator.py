5d2c74b59a504bb544e94c87c28bb050428fd940
nawaphat niemsri <nawaphat.ni@ku.th>
2025-07-27 14:25:24 +0700

    hh

--- assignments/homeworks/hw1/hw1-expense-calculator.py
+++ assignments/homeworks/hw1/hw1-expense-calculator.py
@@ -2,7 +2,8 @@
 #Student : Nawaphat Niemsri 6730202254 Sec.831
 #Date : 26/7/68
 #Purpose : Calculate monthly budget and savings
-#Program
+
+#รับข้อมูลจากผู้ใช้:รายได้และค่าใช้จ่ายต่างๆต่อเดือน
 monthly_income = float(input("What is your monthly income(THB)?:"))
 rent_cost = float(input("What is your Monthly rent/houseing cost ? :"))
 food_budget = int(input("What is your food budget monthly(THB)? :"))
@@ -10,16 +11,18 @@ transportion_cost = float(input("What is your Monthly transportation expenses?:"
 entertainment_budget = int(input("Monthly of Entertainment budget?:"))
 emergency_fund_percent = float(input("Percentage to save for emegency(e.g.,10.5)? :"))
 investment_percent = float(input("Percentage to invest(e.g.,15.0)? :"))
-#Calculate
+
+#คำนวณค่าใช้จ่ายและเงินออมทั้งหมด
 Total_Fixed_Expenses = (rent_cost + transportion_cost)
 Total_Variable_Expenses = (food_budget + entertainment_budget)
 Total_Expenses = (Total_Fixed_Expenses + Total_Variable_Expenses )
-Remaining_Income = (monthly_income - Total_Expenses )                  #คำนวณค่าใช้จ่ายต่างๆ
+Remaining_Income = (monthly_income - Total_Expenses )                  
 Emergency_Fund_Amount = (monthly_income * (emergency_fund_percent / 100)  )
 Investment_Amount = (monthly_income * ( investment_percent / 100))
 Avaliable_for_Saving = (Remaining_Income - Emergency_Fund_Amount - Investment_Amount  )
 Expense_Ratio = (Total_Expenses / monthly_income) * 100
-#Print
+
+#แสดงผลลัพธ์ออกทางหน้าจอ
 print("===MONTHLY BUDGET REPORT===")
 print("Income:",monthly_income,"THB")
 print("Fixed Expenses:",Total_Fixed_Expenses,"THB")
@@ -31,4 +34,4 @@ print("Emergency Fund","(",emergency_fund_percent,"%",")" ,":", Emergency_Fund_A
 print("Investment","(",investment_percent,"%",")" ,":", Investment_Amount ,"THB" )
 print("Avaliable for Savings:",Avaliable_for_Saving,"THB" )
 print("===ANALYSIS===")
-print("Expense Ratio : ",Expense_Ratio,"%")
+print(f"Expense Ratio: {Expense_Ratio:.2f}%")
